2e36d0d56bd4b5ee0a6dd8b3100a210a80a499bb
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2018-11-11 16:39:04 +0900

    Added Custom codepage: Sora-SJIS

---
+++ src/ChList.py
@@ -0,0 +1,63 @@
+
+CHLIST_ENCODEING = 'utf-8'
+BASE_CODEC = 'ms932'
+CHLIST_ENCODEING = 'utf-8'
+
+class Char:
+    def __init__(self, code, glyph):
+        self.code = code
+        self.glyph = glyph
+    def __lt__(self, other):
+        if len(self.code) != len(other.code):
+            return len(self.code) < len(other.code)
+        else:
+            return self.code < other.code
+
+class ChList:
+    def __init__(self, base_codec = BASE_CODEC, chlist_filename = None, chlist_encoding = CHLIST_ENCODEING):
+        self.base_codec = base_codec
+        if chlist_filename:
+            self.open(chlist_filename, chlist_encoding)
+        else:
+            self.ch_list, self.ch_map = [], {}
+
+    def open(self, chlist_filename, chlist_encoding = CHLIST_ENCODEING):
+        ch_map = {}
+        with open(chlist_filename, mode='r', encoding=chlist_encoding) as file:
+            for line in file:
+                line = list(line)
+                while line and line[-1] in '\r\n\t ': line.pop()
+                line = ''.join(line)
+                if not line: continue
+                i = line.find('=', 1)
+                if i < 0:
+                    code, glyph = line, None
+                else:
+                    code, glyph = line[:i], line[i+1:]
+
+                if len(code) == 1:
+                    code = code.encode(self.base_codec)
+                else:
+                    code = bytes.fromhex(code)
+                    test = code.decode(self.base_codec)
+                    if len(test) != 1:
+                        raise UnicodeDecodeError('Decode Error:' + str(code))
+                if not glyph:
+                    glyph = code.decode(self.base_codec)
+                elif len(glyph) > 1:
+                    glyph = chr(int(glyph, 16))
+                
+                ch_map[code] = Char(code, glyph)
+        
+        self.ch_map = ch_map
+        self.ch_list = sorted(ch_map.values())
+    
+    def find(self, code):
+        return self.ch_map.get(code, None)
+
+    def all_chars(self):
+        return self.ch_list
+    
+    def __iter__(self):
+        return iter(self.ch_list)
+
---
+++ src/Sora-SJIS.py
@@ -0,0 +1,60 @@
+import codecs
+import struct, os
+
+from ChList import ChList
+
+CODEC_NAME = 'Sora-SJIS'
+DEFAULT_CHLIST_FILENAME = 'chlist.txt'
+DIR_PY = os.path.split(os.path.realpath(__file__))[0]
+
+CODEC_SJIS = 'ms932'
+CHLIST_ENCODEING = 'utf-8'
+
+class SoraCodec(codecs.Codec):
+    def __init__(self, codec_name, chlist_filename, base_codec_name):
+        self.codec_name = codec_name
+        self.base_codec_name = base_codec_name
+        self.ch_map = get_ch_map(chlist_filename, base_codec_name)
+
+    def encode(self, ucs_str, errors='strict'):
+        output = bytearray()
+
+        for ch in ucs_str:
+            ch = self.ch_map.get(ch, ch)
+            output.extend(ch.encode(self.base_codec_name))
+
+        return bytes(output), len(output)
+
+    def decode(self, byte_array, errors='strict'):
+        raise UnicodeDecodeError('Decode is not supported.')
+
+def get_ch_map(chlist_filename, base_codec_name):
+    ch_map = {}
+    chlist = ChList(base_codec_name, chlist_filename, CHLIST_ENCODEING)
+    for ch in chlist:
+        ch_map[ch.glyph] = ch.code.decode(base_codec_name)
+    return ch_map
+
+def get_regentry(codec_name, chlist_filename, base_codec_name):
+    sora_codec = SoraCodec(codec_name, chlist_filename, base_codec_name)
+    return codecs.CodecInfo(
+        name=sora_codec.codec_name,
+        encode=sora_codec.encode,
+        decode=sora_codec.decode,
+    )
+
+def register(codec_name = CODEC_NAME,
+             chlist_filename = os.path.join(DIR_PY, DEFAULT_CHLIST_FILENAME),
+             base_codec_name = CODEC_SJIS):
+    codec_name = codec_name.lower()
+    base_codec_name = base_codec_name.lower()
+    regentry = get_regentry(codec_name, chlist_filename, base_codec_name)
+    def search_function(codec_name_search):
+        if codec_name_search == codec_name:
+            return regentry
+        else:
+            return None
+    codecs.register(search_function)
+
+def get_codec_name():
+    return CODEC_NAME
